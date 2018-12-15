557f5fdf7282cf664e524bdb62f5b0d1c1e03523
werliton <werlitoncarlos@gmail.com>
2018-12-15 09:04:11 -0300

    Creating relationship Property -> User -> Image

--- airbnb-server/app/Models/Property.js
+++ airbnb-server/app/Models/Property.js
@@ -4,6 +4,14 @@
 const Model = use('Model')
 
 class Property extends Model {
+
+    user(){
+        return this.belongsTo('App/Models/User')
+    }
+
+    images(){
+        return this.hasMany('App/Models/Image')
+    }
 }
 
 module.exports = Property
